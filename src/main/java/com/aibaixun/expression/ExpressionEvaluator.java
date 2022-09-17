dd4cacd26a4c2f47d362ac65947c1dc80ff5837a
王潇 <wxiao1002@yeah.net>
2022-09-17 17:07:03 +0800

    Update ExpressionEvaluator.java

--- src/main/java/com/aibaixun/expression/ExpressionEvaluator.java
+++ src/main/java/com/aibaixun/expression/ExpressionEvaluator.java
@@ -1,4 +1,8 @@
-package com.aibaixun.expression;
+package com.aibaixun.expresstion;
+
+
+import com.aibaixun.expresstion.exception.ExpressException;
+import com.aibaixun.expresstion.node.ExpressNode;
 
 
 public class ExpressionEvaluator implements Evaluator{
@@ -45,7 +49,7 @@ public class ExpressionEvaluator implements Evaluator{
      * @throws Exception  异常信息
      */
     private Object execute(InstructionSet parseResult, ExpressContext<String, Object> expressContext,  boolean isTrace)throws Exception {
-        return null;
+       return InstructionSetRunner.execute(parseResult,expressContext,isTrace);
     }
 
 
@@ -55,6 +59,23 @@ public class ExpressionEvaluator implements Evaluator{
      * @return 指令集合
      */
     private InstructionSet parseInstructionSet(String expression){
-      return null;
+        try {
+            ExpressNode root = expressionParse.parse( expression);
+            return createInstructionSet(root, "main");
+        } catch (ExpressException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new ExpressException("编译异常:\n" + expression, e);
+        }
+    }
+
+    public InstructionSet createInstructionSet(ExpressNode root, String type) throws Exception {
+        InstructionSet result = new InstructionSet(type);
+        createInstructionSet(root, result);
+        return result;
+    }
+
+    public void createInstructionSet(ExpressNode root, InstructionSet result) throws Exception {
+
     }
 }
