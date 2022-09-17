64e3c45b06f95e032f66d622e9993d1c97664797
王潇 <wxiao1002@yeah.net>
2022-09-17 17:05:20 +0800

    Update InstructionSet.java

--- src/main/java/com/aibaixun/expression/InstructionSet.java
+++ src/main/java/com/aibaixun/expression/InstructionSet.java
@@ -1,7 +1,26 @@
-package com.aibaixun.expression;
+package com.aibaixun.expresstion;
+
+
 
 /**
  * 指令集合
  */
-public interface InstructionSet {
+public class InstructionSet {
+
+    private final String type;
+
+    private String name;
+
+
+    public InstructionSet(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public ExpressCallResult execute( boolean isTrace,ExpressContext<String,Object> context) throws Exception {
+        return null;
+    }
 }
