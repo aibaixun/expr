b4933f65c08b0fa94df3fef1ca1a45a5e7ee0ff6
王潇 <wxiao1002@yeah.net>
2022-09-19 11:12:36 +0800

    Update InstructionSet.java

--- src/main/java/com/aibaixun/expression/InstructionSet.java
+++ src/main/java/com/aibaixun/expression/InstructionSet.java
@@ -1,6 +1,9 @@
 package com.aibaixun.expresstion;
 
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * 指令集合
@@ -12,6 +15,9 @@ public class InstructionSet {
     private String name;
 
 
+    private final List<InstructionSet> childInstructionSet = new ArrayList<>();
+
+
     public InstructionSet(String type) {
         this.type = type;
     }
@@ -21,6 +27,29 @@ public class InstructionSet {
     }
 
     public ExpressCallResult execute( boolean isTrace,ExpressContext<String,Object> context) throws Exception {
+
+        if (childInstructionSet.isEmpty()){
+            return ExpressCallResult.empty();
+        }
+        for (InstructionSet instructionSet : childInstructionSet) {
+            instructionSet.execute(isTrace,context);
+        }
         return null;
+    }
+
+    public void addInstructionSet (InstructionSet instructionSet) {
+        this.childInstructionSet.add(instructionSet);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<InstructionSet> getChildInstructionSet() {
+        return childInstructionSet;
     }
 }
