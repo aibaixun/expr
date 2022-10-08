b65809e3422d45c65e0cf2c47d7994ba261e6672
wangxiao <wxiao1002@yeah.net>
2022-10-08 10:01:00 +0800

    build(*): build project

---
+++ src/main/java/com/aibaixun/expr/Expr.java
@@ -0,0 +1,9 @@
+package com.aibaixun.expr;
+
+/**
+ * Expr 解析后的类
+ * @author wang xiao
+ * date 2022/10/8
+ */
+public interface Expr {
+}
---
+++ src/main/java/com/aibaixun/expr/ExprParser.java
@@ -0,0 +1,9 @@
+package com.aibaixun.expr;
+
+/**
+ * exprParser 表达式解析类
+ * @author wang xiao
+ * date 2022/10/8
+ */
+public interface ExprParser {
+}
