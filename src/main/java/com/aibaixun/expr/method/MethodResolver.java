cbf2b9e9be7759a82e9635676857a3cd74b030e8
wangxiao <wxiao1002@yeah.net>
2022-10-10 09:28:33 +0800

    feature(*) covert source to target

---
+++ src/main/java/com/aibaixun/expr/method/MethodResolver.java
@@ -0,0 +1,8 @@
+package com.aibaixun.expr.method;
+
+/**
+ * @author wang xiao
+ * date 2022/10/10
+ */
+public class MethodResolver {
+}
--- src/main/java/com/aibaixun/expr/util/ExprUtil.java
+++ src/main/java/com/aibaixun/expr/util/ExprUtil.java
@@ -1,7 +1,10 @@
 package com.aibaixun.expr.util;
 
 import com.aibaixun.expr.EvalExprContext;
-import com.aibaixun.expr.TypeValue;
+import com.aibaixun.expr.type.TypeConverter;
+import com.aibaixun.expr.type.TypeValue;
+
+import java.util.Objects;
 
 /**
  * @author wang xiao
@@ -10,6 +13,54 @@ import com.aibaixun.expr.TypeValue;
 public abstract class ExprUtil {
 
     public static <T> T convertValue(EvalExprContext context, TypeValue typeValue,Class<?> targetType){
+        Object value = typeValue.getValue();
+        if (Objects.isNull(targetType)){
+            return (T) value;
+        }
+        if (Objects.nonNull(context)){
+            context.getTypeConverter().convertValue(value, typeValue, targetType);
+        }
         return null;
+    }
+
+    public static int toInt(TypeConverter typeConverter, TypeValue TypeValue) {
+        return convertValue(typeConverter, TypeValue, Integer.class);
+    }
+
+    public static boolean toBoolean(TypeConverter typeConverter, TypeValue TypeValue) {
+        return convertValue(typeConverter, TypeValue, Boolean.class);
+    }
+
+    public static double toDouble(TypeConverter typeConverter, TypeValue TypeValue) {
+        return convertValue(typeConverter, TypeValue, Double.class);
+    }
+
+    public static long toLong(TypeConverter typeConverter, TypeValue TypeValue) {
+        return convertValue(typeConverter, TypeValue, Long.class);
+    }
+
+    public static char toChar(TypeConverter typeConverter, TypeValue TypeValue) {
+        return convertValue(typeConverter, TypeValue, Character.class);
+    }
+
+    public static short toShort(TypeConverter typeConverter, TypeValue TypeValue) {
+        return convertValue(typeConverter, TypeValue, Short.class);
+    }
+
+    public static float toFloat(TypeConverter typeConverter, TypeValue TypeValue) {
+        return convertValue(typeConverter, TypeValue, Float.class);
+    }
+
+    public static byte toByte(TypeConverter typeConverter, TypeValue TypeValue) {
+        return convertValue(typeConverter, TypeValue, Byte.class);
+    }
+
+    private static <T> T convertValue(TypeConverter typeConverter, TypeValue TypeValue, Class<T> targetType) {
+        Object result = typeConverter.convertValue(TypeValue.getValue(), TypeValue.getTypeDescriptor(), TypeDescriptor.valueOf(targetType));
+        if (result == null) {
+            throw new IllegalStateException("Null conversion result for value [" + TypeValue.getValue() + "]");
+        } else {
+            return result;
+        }
     }
 }
