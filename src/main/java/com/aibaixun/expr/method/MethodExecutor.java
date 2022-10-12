51f7da428ad8dba774ad74c751cb6ddf6e57fd62
wangxiao <wxiao1002@yeah.net>
2022-10-12 13:42:13 +0800

    feature(*) covert source

--- src/main/java/com/aibaixun/expr/method/MethodExecutor.java
+++ src/main/java/com/aibaixun/expr/method/MethodExecutor.java
@@ -1,8 +1,12 @@
 package com.aibaixun.expr.method;
 
+import com.aibaixun.expr.el.ElExprEvalContext;
+
 /**
  * @author wang xiao
  * date 2022/10/10
  */
-public class MethodExecutor {
+public interface MethodExecutor {
+
+    Object execute(ElExprEvalContext context, Object target, Object ...args);
 }
--- src/main/java/com/aibaixun/expr/method/MethodResolver.java
+++ src/main/java/com/aibaixun/expr/method/MethodResolver.java
@@ -1,8 +1,16 @@
 package com.aibaixun.expr.method;
 
+import com.aibaixun.expr.ExprEvalContext;
+import com.aibaixun.expr.el.ElExprEvalContext;
+
+import java.util.List;
+
 /**
  * @author wang xiao
  * date 2022/10/10
  */
-public class MethodResolver {
+public interface MethodResolver {
+
+
+    MethodExecutor resolve(ExprEvalContext context, Object targetObject, String name, List<Class<?>> argumentTypes);
 }
