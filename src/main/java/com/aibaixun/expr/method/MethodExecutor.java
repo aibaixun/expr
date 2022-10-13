3a487735020e8905d759d8c8b98ab4b0f6d262a8
wangxiao <wxiao1002@yeah.net>
2022-10-13 22:18:58 +0800

    feature(*) add method executor

--- src/main/java/com/aibaixun/expr/method/MethodExecutor.java
+++ src/main/java/com/aibaixun/expr/method/MethodExecutor.java
@@ -3,10 +3,18 @@ package com.aibaixun.expr.method;
 import com.aibaixun.expr.el.ElExprEvalContext;
 
 /**
+ * 方法执行
  * @author wang xiao
  * date 2022/10/10
  */
 public interface MethodExecutor {
 
+    /**
+     * 执行方法
+     * @param context 执行上下文
+     * @param target target
+     * @param args 参数
+     * @return 执行结果
+     */
     Object execute(ElExprEvalContext context, Object target, Object ...args);
 }
--- src/main/java/com/aibaixun/expr/method/MethodResolver.java
+++ src/main/java/com/aibaixun/expr/method/MethodResolver.java
@@ -1,7 +1,6 @@
 package com.aibaixun.expr.method;
 
 import com.aibaixun.expr.ExprEvalContext;
-import com.aibaixun.expr.el.ElExprEvalContext;
 
 import java.util.List;
 
@@ -11,6 +10,13 @@ import java.util.List;
  */
 public interface MethodResolver {
 
-
+    /**
+     * 解析 方法
+     * @param context 运行上下文
+     * @param targetObject 目标对象
+     * @param name 方法名称
+     * @param argumentTypes 类型
+     * @return 方法执行器材
+     */
     MethodExecutor resolve(ExprEvalContext context, Object targetObject, String name, List<Class<?>> argumentTypes);
 }
