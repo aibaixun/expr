cd6ab70d40794c2f5266142fc32d75e6bcb90320
wangxiao <wxiao1002@yeah.net>
2022-10-17 21:45:41 +0800

    feature(*) add token

--- src/main/java/com/aibaixun/expr/el/standard/Token.java
+++ src/main/java/com/aibaixun/expr/el/standard/Token.java
@@ -4,5 +4,33 @@ package com.aibaixun.expr.el.standard;
  * @author wang xiao
  * date 2022/10/17
  */
-public class Token {
+ class Token {
+
+    String data;
+    int startPos;
+    int endPos;
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public int getStartPos() {
+        return startPos;
+    }
+
+    public void setStartPos(int startPos) {
+        this.startPos = startPos;
+    }
+
+    public int getEndPos() {
+        return endPos;
+    }
+
+    public void setEndPos(int endPos) {
+        this.endPos = endPos;
+    }
 }
---
+++ src/main/java/com/aibaixun/expr/el/standard/TokenType.java
@@ -0,0 +1,8 @@
+package com.aibaixun.expr.el.standard;
+
+/**
+ * @author wang xiao
+ * date 2022/10/17
+ */
+public enum TokenType {
+}
