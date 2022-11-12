82c815ac50651ac8e8d50bcdb2be3aae40e6ee6c
wangxiao <wxiao1002@yeah.net>
2022-11-12 22:31:15 +0800

    feature(*) 暂时放弃实现

--- src/main/java/com/aibaixun/expr/el/standard/InternalElExpressionParser.java
+++ src/main/java/com/aibaixun/expr/el/standard/InternalElExpressionParser.java
@@ -42,46 +42,11 @@ public class InternalElExpressionParser extends TemplateExprParser {
         this.tokenListLength = this.tokenList.size();
         this.tokenListPointer = 0;
         this.constructedElNodes.clear();
-        ElNodeImpl ast = eatExpression();
+        ElNodeImpl ast = null;
         return new ElExpr(expression,ast);
     }
 
-    private ElNodeImpl eatExpression() {
-        ElNodeImpl expr = eatLogicalOrExpression();
-        Token t = peekToken();
-        if (t != null) {
-            if (t.kind == TokenKind.ASSIGN) {  // a=b
-                if (expr == null) {
-                    expr = new NullLiteral(t.startPos - 1, t.endPos - 1);
-                }
-                nextToken();
-                ElNodeImpl assignedValue = eatLogicalOrExpression();
-                return new Assign(t.startPos, t.endPos, expr, assignedValue);
-            }
-            if (t.kind == TokenKind.ELVIS) {  // a?:b (a if it isn't null, otherwise b)
-                if (expr == null) {
-                    expr = new NullLiteral(t.startPos - 1, t.endPos - 2);
-                }
-                nextToken();  // elvis has left the building
-                ElNodeImpl valueIfNull = eatExpression();
-                if (valueIfNull == null) {
-                    valueIfNull = new NullLiteral(t.startPos + 1, t.endPos + 1);
-                }
-                return new Elvis(t.startPos, t.endPos, expr, valueIfNull);
-            }
-            if (t.kind == TokenKind.QMARK) {  // a?b:c
-                if (expr == null) {
-                    expr = new NullLiteral(t.startPos - 1, t.endPos - 1);
-                }
-                nextToken();
-                ElNodeImpl ifTrueExprValue = eatExpression();
-                eatToken(TokenKind.COLON);
-                ElNodeImpl ifFalseExprValue = eatExpression();
-                return new Ternary(t.startPos, t.endPos, expr, ifTrueExprValue, ifFalseExprValue);
-            }
-        }
-        return expr;
-    }
+
 
 
     public ElExprParserConfig getParserConfig() {
